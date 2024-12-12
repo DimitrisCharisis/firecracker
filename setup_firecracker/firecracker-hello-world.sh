4214f0c9dcea0a97005fc3b232f7b95cf52e341c
Dimitris Charisis <dchar@cslab.ece.ntua.gr>
2024-12-12 19:16:33 +0200

    WiP
    Signed-off-by: Dimitris Charisis <dchar@cslab.ece.ntua.gr>

--- setup_firecracker/firecracker-hello-world.sh
+++ setup_firecracker/firecracker-hello-world.sh
@@ -10,26 +10,27 @@ set -eu
 # [ -e hello-id_rsa ] || wget -O hello-id_rsa https://raw.githubusercontent.com/firecracker-microvm/firecracker-demo/ec271b1e5ffc55bd0bf0632d5260e96ed54b5c0c/xenial.rootfs.id_rsa
 
 arch=`uname -m`
-dest_kernel="hello-vmlinux.bin"
+# dest_kernel="hello-vmlinux.bin"
+dest_kernel="vmlinux"
 dest_rootfs="hello-rootfs.ext4"
 image_bucket_url="https://s3.amazonaws.com/spec.ccfc.min/img/quickstart_guide/$arch"
 
 if [ ${arch} = "x86_64" ]; then
-    kernel="${image_bucket_url}/kernels/vmlinux.bin"
+#     kernel="${image_bucket_url}/kernels/vmlinux.bin"
     rootfs="${image_bucket_url}/rootfs/bionic.rootfs.ext4"
 elif [ ${arch} = "aarch64" ]; then
-    kernel="${image_bucket_url}/kernels/vmlinux.bin"
+#     kernel="${image_bucket_url}/kernels/vmlinux.bin"
     rootfs="${image_bucket_url}/rootfs/bionic.rootfs.ext4"
 else
     echo "Cannot run firecracker on $arch architecture!"
     exit 1
 fi
 
-if [ ! -f $dest_kernel ]; then
-    echo "Kernel not found, downloading $kernel..."
-    curl -fsSL -o $dest_kernel $kernel
-    echo "Saved kernel file to $dest_kernel."
-fi
+# if [ ! -f $dest_kernel ]; then
+#     echo "Kernel not found, downloading $kernel..."
+#     curl -fsSL -o $dest_kernel $kernel
+#     echo "Saved kernel file to $dest_kernel."
+# fi
 
 if [ ! -f $dest_rootfs ]; then
     echo "Rootfs not found, downloading $rootfs..."
@@ -65,7 +66,7 @@ ip link set dev "$TAP_DEV" up
 cat <<EOF > vmconfig.json
 {
   "boot-source": {
-    "kernel_image_path": "hello-vmlinux.bin",
+    "kernel_image_path": "$dest_kernel",
     "boot_args": "$KERNEL_BOOT_ARGS"
   },
   "drives": [
